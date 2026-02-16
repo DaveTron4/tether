16897f983fa643556f439bee13f1ee863d870556
DaveTron4 <davidtnt970@gmail.com>
2026-02-15 23:32:46 -0500

    Started working on clients page

--- frontend/src/app/features/crm/components/client-table/client-table.ts
+++ frontend/src/app/features/crm/components/client-table/client-table.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LucideAngularModule, MoreHorizontal, Edit, Trash2, LUCIDE_ICONS, LucideIconProvider } from 'lucide-angular';
+import { Client } from '../../../../shared/models/client.interface';
 
 @Component({
   selector: 'app-client-table',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, LucideAngularModule], // Don't forget imports!
   templateUrl: './client-table.html',
   styleUrl: './client-table.css',
+  providers: [
+    {
+      provide: LUCIDE_ICONS,
+      multi: true,
+      useValue: new LucideIconProvider({ MoreHorizontal, Edit, Trash2 })
+    }
+  ]
 })
-export class ClientTable {
-
+export class ClientTable{
+  @Input() clients: Client[] = [];
+  @Output() onEdit = new EventEmitter<Client>();
 }
--- frontend/src/app/features/crm/pages/client-list/client-list.ts
+++ frontend/src/app/features/crm/pages/client-list/client-list.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LucideAngularModule, MoreHorizontal, Edit, Trash2, LUCIDE_ICONS, LucideIconProvider } from 'lucide-angular';
+import { Client } from '../../../../shared/models/client.interface';
 
 @Component({
   selector: 'app-client-list',
-  imports: [],
+  imports: [CommonModule, LucideAngularModule],
   templateUrl: './client-list.html',
   styleUrl: './client-list.css',
+  providers: [
+    {
+      provide: LUCIDE_ICONS,
+      multi: true,
+      useValue: new LucideIconProvider({ MoreHorizontal, Edit, Trash2 })
+    }
+  ]
 })
 export class ClientList {
 
